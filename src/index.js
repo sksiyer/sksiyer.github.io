453abced22de538af4e022ccdc2a8a74c3a03607
sksiyer <sarahksiyer@gmail.com>
2018-08-20 21:25:46 +0100

    gh pages symbolic link fix

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import _ from 'lodash';
 import "./style/style.scss";
 
 
-function component() {
-        let element = document.createElement('div');
-
-        element.innerHTML = _.join(['Hello', 'Boo'], ' ');
-
-        return element;
-    }
-
-document.body.appendChild(component());
+// function component() {
+//         let element = document.createElement('div');
+//
+//         element.innerHTML = _.join(['Hello', 'Boo'], ' ');
+//
+//         return element;
+//     }
+//
+// document.body.appendChild(component());
