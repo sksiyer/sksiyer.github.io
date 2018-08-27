dc799de62f3c6b8de8c3b5812c4f1b4a18c4dd2f
sksiyer <sarahksiyer@gmail.com>
2018-08-27 20:46:44 +0100

    index js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -6,7 +7,12 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
     },
-    devtool: "source-map", // any "source-map"-like devtool is possible
+    devtool: "source-map", // any "source-map"-like devtool is possible,
+    plugins: [
+        new HtmlWebpackPlugin({
+            favicon: 'src/img/favicon.ico'
+        })
+    ],
     module: {
         rules: [
             {
