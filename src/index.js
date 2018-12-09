b87c5b173041760ced3d6d5984ee1be9c18ecee7
sksiyer <sarahksiyer@gmail.com>
2018-12-09 17:18:54 +0000

    fixed untracked files

--- src/index.js
+++ src/index.js
@@ -6,3 +6,9 @@ import styles from "./style/style.scss";
 console.log('Colours pitch: ' + colours.pitch);
 console.log('styles pitch: ' + styles.pitch);
 console.log('styles fuschia: ' + styles.fushcia);
+
+//ackage json dependables removed
+//    "lodash": "^4.17.10",
+//    "jquery": "^3.3.1",
+//    "popper.js": "^1.14.4",
+//    "bootstrap": "^4.0.0-alpha.6",
