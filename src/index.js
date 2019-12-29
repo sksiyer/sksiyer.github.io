2f32bff32faeb859c1a4eb1bfd4996fcc7cec056
sksiyer <sarahksiyer@gmail.com>
2019-12-29 17:12:51 +0000

    new github commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,3 @@
 import "./style/style.scss";
 import colours from './style/_colours.scss';
 import styles from "./style/style.scss";
-
-
-console.log('Colours pitch: ' + colours.pitch);
-console.log('styles pitch: ' + styles.pitch);
-console.log('styles fuschia: ' + styles.fushcia);
-
-//ackage json dependables removed
-//    "lodash": "^4.17.10",
-//    "jquery": "^3.3.1",
-//    "popper.js": "^1.14.4",
-//    "bootstrap": "^4.0.0-alpha.6",
